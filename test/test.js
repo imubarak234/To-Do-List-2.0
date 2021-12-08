b531b7a9859844b23f69bf3d7c7a47c83529b0a3
Manezeu Patricia <pmanezeu@gmail.com>
2021-12-08 15:56:56 +0100

    Addesd test case for remove at index and at description

--- test/test.js
+++ test/test.js
@@ -4,9 +4,9 @@
 
 import List from '../src/listClass.js';
 
-const testlist = new List();
-
 describe('Testing the add function and process', () => {
+  const testlist = new List();
+
   test('Adds element to list properly', () => {
     testlist.addToEnd('eating', true);
     expect(testlist.Lists).toHaveLength(1);
@@ -17,14 +17,13 @@ describe('Testing the add function and process', () => {
     expect(testlist.Lists).toEqual([{
       description: 'eating',
       completed: true,
-      index: 1
-    },{
+      index: 1,
+    }, {
       description: 'coding',
       completed: false,
-      index: 2
+      index: 2,
     }]);
   });
-
 
   test('Testing the boolean completed', () => {
     testlist.addToEnd('Come home early', false);
@@ -33,10 +32,19 @@ describe('Testing the add function and process', () => {
 });
 
 describe('Testing the remove functions', () => {
-  test('Check if the element is removed', () => {
-    testlist.addToEnd('Walk the dog', true);
+  const testlist = new List();
+
+  testlist.addToEnd('Walk the dog', false);
+  testlist.addToEnd('Finish my homeworks', false);
+
+  test('Check if the element is removed by index', () => {
     const count = testlist.Lists.length;
-    testlist.removeAt(0);
+    testlist.removeAt(testlist.Lists[0].index);
     expect(testlist.Lists.length).toBeLessThan(count);
+  });
+
+  test('Check if the element is removed by description', () => {
+    testlist.removeAt('Finish my homeworks');
+    expect(testlist.Lists).toHaveLength(0);
   });
 });
