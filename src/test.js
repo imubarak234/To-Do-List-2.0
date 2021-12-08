9ef94ea01d4cb3ce974672bb8284cd1affa76c53
Mubarak <imubarak2424@gmail.com>
2021-12-08 14:46:05 +0100

    linter

--- src/test.js
+++ src/test.js
@@ -8,25 +8,21 @@ const testlist = new List();
 
 describe('Testing the add function and process', () => {
   test('Adds element to list properly', () => {
-    testlist.addToEnd('eating', true)
+    testlist.addToEnd('eating', true);
     expect(testlist.Lists).toHaveLength(1);
   });
 
   test('Testing the boolean completed', () => {
     testlist.addToEnd('Come home early', false);
     expect((testlist.returnDes('Come home early')).completed).toBeFalsy();
-  })
-})
+  });
+});
 
 describe('Testing the remove functions', () => {
   test('Check if the element is removed', () => {
     testlist.addToEnd('Walk the dog', true);
-    let count = testlist.Lists.length;
-    testlist.removeAt(0)
+    const count = testlist.Lists.length;
+    testlist.removeAt(0);
     expect(testlist.Lists.length).toBeLessThan(count);
   });
-})
-
-
-  
-  
+});
