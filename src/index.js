c86199f7c6a93f2c9bf0c38df0b4c26b99b654c2
Mubarak <imubarak2424@gmail.com>
2022-09-22 10:50:57 +0100

    few tests

--- src/index.js
+++ src/index.js
@@ -27,6 +27,35 @@ function populate() {
   localStorage.setItem('instances', JSON.stringify(instances));
 }
 
+// const test = document.getElementById('test-button');
+
+// test.addEventListener('click' , (e) => {
+
+   const sgMail = require('@sendgrid/mail');
+
+// // import sgMail from './node_modules/@sendgrid/mail/index.js'
+// // import sgMail from '@sendgrid/mail';
+// sgMail.setApiKey('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');
+
+//   const msg = {
+//     to: 'shithappens242@gmail.com', // Change to your recipient
+//     from: 'imubarak2424@gmail.com', // Change to your verified sender
+//     subject: 'Sending with SendGrid is Fun',
+//     text: 'and easy to do anywhere, even with Node.js',
+//     html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+//   }
+  
+//   sgMail
+//     .send(msg)
+//     .then(() => {
+//       console.log('Email sent')
+//     })
+//     .catch((error) => {
+//       console.error(error)
+//     })
+
+// })
+
 const addListToPages = (description, next) => {
   to_Do_List.addToEnd(description, next);
   const section = document.querySelector('.main-list');
